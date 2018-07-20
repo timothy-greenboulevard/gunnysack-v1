5f06fe274a7d69b5ca7ddca958c7f2849349022b
timothy-greenboulevard <timothy@greenboulevard.com.sg>
2018-07-20 17:30:23 +0800

    added postgresql module

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
 /* GET landing page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +15,11 @@ router.get('/login', function(req, res, next) {
 //GET home page
 router.get('/home',function(req, res, next){
   res.render('home',{ title: 'Home - Gunny Sack' })
+});
+
+//GET DB ***TESTING REMOVE AFTER USE***
+router.get('/db',function(req, res, next){
+  res.render('dbTest',{ title: 'Testing DB' })
 });
 
 
